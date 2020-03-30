8923867f7fdc9db1d3247a606ca1f2e8ca6505df
Bukhaar M <bukharim96@gmail.com>
2020-03-30 23:21:49 +0200

    workflow update

--- index.js
+++ index.js
@@ -5,18 +5,8 @@ try {
   const authToken = core.getInput("authToken");
   const octokit = new github.GitHub(authToken);
   const payload = github.context.payload;
-  const username = payload.head_commit.author.username;
-  const repo = payload.repository.name;
-  const commitData = {
-    owner: username,
-    repo: repo,
-    path: `build/${username}.first-post.html`,
-    // sha: "ee61611dd820f9d275fe35f66216595b71c0535f",
-    message: "[NEW POST]",
-    content: Buffer.from(`<!-- dummy post -->`).toString("base64")
-  };
 
-  octokit.repos.createOrUpdateFile(commitData);
+  if (payload.created) createFile(authToken, payload);
 
   //   core.setOutput("time", time);
   // Get the JSON webhook payload for the event that triggered the workflow
@@ -24,4 +14,19 @@ try {
   console.log(`The event payload: ${payloadData}`);
 } catch (error) {
   core.setFailed(error.message);
+}
+
+function createFile(authToken, payload) {
+  const username = payload.head_commit.author.username;
+  const repo = payload.repository.name;
+  const commitData = {
+    owner: username,
+    repo: repo,
+    path: `build/${username}.third-post.html`,
+    // sha: "ee61611dd820f9d275fe35f66216595b71c0535f",
+    message: "[NEW POST]",
+    content: Buffer.from(`<!-- dummy post -->`).toString("base64")
+  };
+
+  octokit.repos.createOrUpdateFile(commitData);
 }
