8dd831295feff8e76d3e6adc5d1325b4c6e88b87
Bukhaar M <bukharim96@gmail.com>
2020-04-02 15:37:35 +0200

    workflow update

--- index.js
+++ index.js
@@ -32,7 +32,9 @@ async function handleNewPosts(filesAdded, githubToken, payload) {
 
   console.log(`filesAdded: ${filesAdded}`);
 
-  filesAdded.forEach(filePath => {
+  for (const i in filesAdded) {
+    const filePath = filesAdded[i];
+
     console.log(`...reading ${filePath}`);
     const result = await octokit.repos.getContents({
       owner: username,
@@ -42,7 +44,7 @@ async function handleNewPosts(filesAdded, githubToken, payload) {
     // content will be base64 encoded
     const content = Buffer.from(result.data.content, "base64").toString();
     console.log(`${filePath}: ${content}`);
-  }); 
+  }
 
   // update file
   // const commitData = {
