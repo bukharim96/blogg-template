13dd88d3985e1a0347fc6b32e097e388cbb92001
Bukhaar M <bukharim96@gmail.com>
2020-04-02 15:12:27 +0200

    workflow update

--- index.js
+++ index.js
@@ -8,32 +8,44 @@ try {
   const filesRemoved = core.getInput("files_removed");
   const payload = github.context.payload;
 
-  console.log(`filesAdded: ${filesAdded}`);
-  console.log(`filesModified: ${filesModified}`);
-  console.log(`filesRemoved: ${filesRemoved}`);
-  
-//   createFile(githubToken, payload);
+  if (filesAdded.length) handleNewPosts(filesAdded, githubToken, payload);
+  // if (filesModified.length)
+  // if (filesRemoved.length)
 
   //   core.setOutput("time", time);
   // Get the JSON webhook payload for the event that triggered the workflow
-  const payloadData = JSON.stringify(github, undefined, 2);
+  const payloadData = JSON.stringify(payload, undefined, 2);
   console.log(`The event payload: ${payloadData}`);
 } catch (error) {
   core.setFailed(error.message);
 }
 
-function createFile(githubToken, payload) {
+function handleNewPosts(filesAdded, githubToken, payload) {
   const octokit = new github.GitHub(githubToken);
   const username = payload.head_commit.author.username;
   const repo = payload.repository.name;
-  const commitData = {
-    owner: username,
-    repo: repo,
-    path: `build/${username}.third-post.html`,
-    // sha: "ee61611dd820f9d275fe35f66216595b71c0535f",
-    message: "[NEW POST]",
-    content: Buffer.from(`<!-- dummy post -->`).toString("base64")
-  };
 
-  octokit.repos.createOrUpdateFile(commitData);
+  for (const filePath in filesAdded) {
+    const result = await octokit.repos
+      .getContent({
+        owner: username,
+        repo: repo,
+        path: filePath
+      });
+    // content will be base64 encoded
+    const content = Buffer.from(result.data.content, "base64").toString();
+    console.log(`${filePath}: ${content}`);
+  }
+
+  // update file
+  // const commitData = {
+  //   owner: username,
+  //   repo: repo,
+  //   path: `build/${username}.third-post.html`,
+  //   // sha: "ee61611dd820f9d275fe35f66216595b71c0535f",
+  //   message: "[NEW POST]",
+  //   content: Buffer.from(`<!-- dummy post -->`).toString("base64")
+  // };
+
+  // octokit.repos.createOrUpdateFile(commitData);
 }
