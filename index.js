19ec7dcaa32caba56de770e7a2d5ebe99b9232a9
Bukhaar M <bukharim96@gmail.com>
2020-04-07 11:27:22 +0200

    workflow update

--- index.js
+++ index.js
@@ -70,7 +70,7 @@ async function handleNewPosts(filesAdded, githubToken, payload) {
     owner: username,
     repo: repo,
     base: "master",
-    head: "master",
+    head: "blogg-public",
     changes: changes
   })
     .then(result => {
